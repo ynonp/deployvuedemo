3bd7ff79589d7ca1f52905ec5f98065336ae0f8e
ynonp <ynonperek@gmail.com>
2025-02-04 14:56:12 +0200

    add pages

---
+++ pages/blog.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h1>Welcome to my blog</h1>
+</template>
+
+<style lang="css" scoped>
+  h1 { color: red; }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <h1>Home Page</h1>
+  <p>check out my 
+    <a href="/blog">Blog</a>
+  </p>
+</template>
